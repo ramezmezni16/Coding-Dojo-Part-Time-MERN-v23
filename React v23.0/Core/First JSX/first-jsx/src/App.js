fcd79d03472587b38b1763a4485fa2ad3fa00282
ramezmezni16 <ramez.nabila@gmail.com>
2024-05-18 09:15:19 +0100

    changes

---
+++ React v23.0/Core/First JSX/first-jsx/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>things I need to do:</h3>
+      <p>* Learn React</p>
+      <p>* Climb Mt. Everest</p>
+      <p>* Run a marathon</p>
+      <p>* Feed the dogs</p>
+    </div>
+  );
+}
+
+export default App;

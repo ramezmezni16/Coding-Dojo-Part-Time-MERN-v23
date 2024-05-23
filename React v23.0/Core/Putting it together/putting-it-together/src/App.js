5f148189d6c135ef27ca5ad889a385fea01138b7
ramezmezni16 <164899864+ramezmezni16@users.noreply.github.com>
2024-05-23 14:45:15 +0100

    Update App.js

--- React v23.0/Core/Putting it together/putting-it-together/src/App.js
+++ React v23.0/Core/Putting it together/putting-it-together/src/App.js
@@ -1,6 +1,3 @@
-
-
-// App.js
 import React from 'react';
 import './App.css';
 import PersonComponent from './components/MyNewComponent';
@@ -9,7 +6,6 @@ const peopleArr = [
     { "firstName": "Jane", "lastName": "Doe", "age": 45, "hairColor": "Black" },
     { "firstName": "John", "lastName": "Smith", "age": 88, "hairColor": "Brown" },
 ];
-
 function App() {
     return (
         <div className="App">
@@ -25,5 +21,4 @@ function App() {
         </div>
     );
 }
-
 export default App;

b85cd16b406686aec0f301ec7636a48d6e8a28e7
ramezmezni16 <ramez.nabila@gmail.com>
2024-05-23 13:19:58 +0100

    changes

---
+++ React v23.0/Core/Putting it together/putting-it-together/src/App.js
@@ -0,0 +1,29 @@
+
+
+// App.js
+import React from 'react';
+import './App.css';
+import PersonComponent from './components/MyNewComponent';
+
+const peopleArr = [
+    { "firstName": "Jane", "lastName": "Doe", "age": 45, "hairColor": "Black" },
+    { "firstName": "John", "lastName": "Smith", "age": 88, "hairColor": "Brown" },
+];
+
+function App() {
+    return (
+        <div className="App">
+            {peopleArr.map((person, index) => (
+                <PersonComponent
+                    key={index}
+                    firstName={person.firstName}
+                    lastName={person.lastName}
+                    age={person.age}
+                    hairColor={person.hairColor}
+                />
+            ))}
+        </div>
+    );
+}
+
+export default App;
---
+++ React v23.0/Core/Putting it together/putting-it-together/src/components/MyNewComponent.js
@@ -0,0 +1,28 @@
+// components/MyNewComponent.js
+import React, { Component } from 'react';
+
+class PersonComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: props.age
+        };
+    }
+
+    birthdayButton = () => {
+        this.setState(prevState => ({ age: prevState.age + 1 }));
+    };
+
+    render() {
+        return (
+            <div className="person-card">
+                <h1>{this.props.lastName}, {this.props.firstName}</h1>
+                <p>Age: {this.state.age}</p>
+                <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={this.birthdayButton}>Birthday Button</button>
+            </div>
+        );
+    }
+}
+
+export default PersonComponent;
